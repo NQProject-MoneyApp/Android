0f34335cd7e04657532f613e632e67f6698a0d2a
Szymon Gesicki <szym.gesicki@gmail.com>
2021-07-28 22:09:08 +0200

    fixed input fields

--- app/src/main/java/com/nqproject/MoneyApp/ui/screens/auth/AuthInputFields.kt
+++ app/src/main/java/com/nqproject/MoneyApp/ui/screens/auth/AuthInputFields.kt
@@ -1,124 +1,57 @@
 package com.nqproject.MoneyApp.ui.screens.auth
 
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.NativeKeyEvent
-import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import com.nqproject.MoneyApp.ui.theme.AppTheme
 
 @Composable
 fun AuthInputFields(usernameState: MutableState<String>, passwordState: MutableState<String>, onDone: () -> Unit, emailState: MutableState<String>? = null) {
 
-    val focusRequester = remember { FocusRequester() }
+    val emailFocusRequester = remember { FocusRequester() }
+    val passwordFocusRequester = remember { FocusRequester() }
+
     val focusManager = LocalFocusManager.current
 
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .onKeyEvent {
-                if (it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_TAB ||
-                    it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_ENTER){
-                    focusRequester.requestFocus()
-                    true //true -> consumed
-                } else false },
-        value = usernameState.value,
-        onValueChange = { newValue ->
-            usernameState.value = newValue.filter { it != '\n' && it != '\t' }
-        },
-        label = { Text("Username", color = AppTheme.colors.hintText) },
-        shape = RoundedCornerShape(10.dp),
-        singleLine = true,
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-        ),
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Next
-        ),
-        keyboardActions = KeyboardActions(onNext = {
-            focusRequester.requestFocus()
+    InputField(
+        fieldState = usernameState,
+        focusRequester = FocusRequester(),
+        keyboardType = KeyboardType.Text,
+        placeholder = "Username",
+        focusRequesterAction = {
+            emailState?.let {
+                emailFocusRequester.requestFocus()
+            } ?: passwordFocusRequester.requestFocus()
         })
-    )
-
-    if (emailState != null) {
-        Spacer(modifier = Modifier.height(21.dp))
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth(),
-            value = emailState.value,
-            onValueChange = { newValue ->
-                emailState.value = newValue
-            },
-            label = { Text("Email", color = AppTheme.colors.hintText) },
-            shape = RoundedCornerShape(10.dp),
-            singleLine = true,
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = MaterialTheme.colors.surface,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-            )
-        )
-    }
 
     Spacer(modifier = Modifier.height(21.dp))
 
-    TextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .focusRequester(focusRequester)
-            .onKeyEvent {
-                if (it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_TAB ||
-                    it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_ENTER){
-                    onDone()
-                    true //true -> consumed
-                } else false },
-        value = passwordState.value,
-        onValueChange = { newValue ->
-            passwordState.value = newValue.filter { it != '\n' && it != '\t' }
-        },
-        label = { Text("Password", color = AppTheme.colors.hintText) },
-        shape = RoundedCornerShape(10.dp),
-        singleLine = true,
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = MaterialTheme.colors.surface,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-        ),
-        visualTransformation = PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Password,
-            imeAction = ImeAction.Done,
-        ),
-        keyboardActions = KeyboardActions(onDone = {
+    if (emailState != null) {
+        InputField(
+            focusRequester = emailFocusRequester,
+            fieldState = emailState,
+            keyboardType = KeyboardType.Email,
+            placeholder = "Email",
+            focusRequesterAction = {
+                passwordFocusRequester.requestFocus()
+            })
+        Spacer(modifier = Modifier.height(21.dp))
+    }
+
+    InputField(
+        focusRequester = passwordFocusRequester,
+        fieldState = passwordState,
+        keyboardType = KeyboardType.Password,
+        placeholder = "Password",
+        focusRequesterAction = {
             onDone()
             focusManager.clearFocus()
-        })
+        },
     )
 }
---
+++ app/src/main/java/com/nqproject/MoneyApp/ui/screens/auth/InputField.kt
@@ -0,0 +1,69 @@
+package com.nqproject.MoneyApp.ui.screens.auth
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.nqproject.MoneyApp.ui.theme.AppTheme
+
+
+@Composable
+fun InputField(focusRequester: FocusRequester, fieldState: MutableState<String>, focusRequesterAction: () -> Unit, placeholder: String, keyboardType: KeyboardType) {
+
+    val visualTransformation = if (keyboardType == KeyboardType.Password) {
+        PasswordVisualTransformation()
+    } else {
+        VisualTransformation.None
+    }
+
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester)
+            .onKeyEvent {
+                if (it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_TAB ||
+                    it.nativeKeyEvent.keyCode == NativeKeyEvent.KEYCODE_ENTER
+                ) {
+                    focusRequesterAction()
+                    true //true -> consumed
+                } else false
+            },
+        value = fieldState.value,
+        onValueChange = { newValue ->
+            fieldState.value = newValue.filter { it != '\n' && it != '\t' }
+        },
+        label = { Text(placeholder, color = AppTheme.colors.hintText) },
+        shape = RoundedCornerShape(10.dp),
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        visualTransformation = visualTransformation,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = keyboardType,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(onNext = {
+            focusRequesterAction()
+        })
+    )
+}
