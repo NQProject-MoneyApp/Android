f1e8724654462d497dc7493baa44fb3ebda52911
SpeeritX <Jedrek.185@gmail.com>
2021-08-03 17:58:07 +0200

    Add error message when no expense participants were chosen

--- app/src/main/java/com/nqproject/MoneyApp/ui/screens/add_expense/AddExpenseForm.kt
+++ app/src/main/java/com/nqproject/MoneyApp/ui/screens/add_expense/AddExpenseForm.kt
@@ -3,7 +3,6 @@ package com.nqproject.MoneyApp.ui.screens.add_expense
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,10 +17,9 @@ import com.nqproject.MoneyApp.ui.screens.auth.InputField
 import com.nqproject.MoneyApp.ui.screens.auth.InputFieldValidator
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.nqproject.MoneyApp.ui.screens.add_group.AddUserComponent
+import com.nqproject.MoneyApp.components.ChooseUsersComponent
+import com.nqproject.MoneyApp.repository.User
 import java.util.*
 
 
@@ -35,8 +33,6 @@ fun AddExpenseForm(
     val viewModel = viewModel<AddExpenseViewModel>()
     val chosenMembers = viewModel.chosenParticipants.observeAsState(emptyList()).value
     val groupMembers = viewModel.groupMembers.observeAsState(emptyList()).value
-
-
 
     val expenseName = remember { mutableStateOf(defaultName) }
     val expenseAmount = remember {
@@ -52,7 +48,7 @@ fun AddExpenseForm(
     val focusManager = LocalFocusManager.current
 
     val nameValidator by remember {
-        mutableStateOf(InputFieldValidator {
+        mutableStateOf(InputFieldValidator<String> {
             when {
                 it.isEmpty() -> "Enter an expense name"
                 else -> ""
@@ -61,11 +57,20 @@ fun AddExpenseForm(
     }
 
     val amountValidator by remember {
-        mutableStateOf(InputFieldValidator {
+        mutableStateOf(InputFieldValidator<String> {
             val value = it.toFloatOrNull() ?: 0f
             when {
                 value == 0f -> "Enter an expense amount"
                 value < 0f -> "Expense amount must be greater than zero"
+                else -> ""
+            }
+        })
+    }
+
+    val usersValidator by remember {
+        mutableStateOf(InputFieldValidator<List<User>> {
+            when {
+                it.isEmpty() -> "Choose expense participants"
                 else -> ""
             }
         })
@@ -99,37 +104,15 @@ fun AddExpenseForm(
 
     Spacer(modifier = Modifier.height(5.dp))
 
-    Card(
-        backgroundColor = MaterialTheme.colors.secondary,
-        shape = RoundedCornerShape(15),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-
-    ) {
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text("Members", color = Color.White)
-            Spacer(modifier = Modifier.height(16.dp))
-
-
-            groupMembers.forEach {
-                AddUserComponent(
-                    user = it,
-                    check=chosenMembers.contains(it),
-                    didPressComponent = {
-                        if (chosenMembers.contains(it)) {
-                            viewModel.removeChosenMember(it)
-                        } else {
-                            viewModel.addChosenMember(it)
-                        }
-                    })
-                Spacer(modifier = Modifier.height(8.dp))
-            }
-        }
+    if(groupMembers.isNotEmpty()) {
+        ChooseUsersComponent(
+            title = "Participants",
+            groupMembers = groupMembers,
+            chosenMembers = chosenMembers,
+            onAddUser = { viewModel.addChosenMember(it) },
+            onRemoveUser = { viewModel.removeChosenMember(it) },
+            validator = usersValidator,
+        )
     }
 
     Spacer(modifier = Modifier.height(5.dp))
@@ -143,7 +126,8 @@ fun AddExpenseForm(
         onClick = {
             nameValidator.validate(expenseName.value)
             amountValidator.validate(expenseAmount.value)
-            if (!nameValidator.isError() and !amountValidator.isError())
+            usersValidator.validate(chosenMembers)
+            if (!nameValidator.isError() and !amountValidator.isError() and !usersValidator.isError())
                 onSave(expenseName.value, expenseAmount.value.toFloatOrNull() ?: 0f)
         }) {
         Text("Save", style = MaterialTheme.typography.h4)
