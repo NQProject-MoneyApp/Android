e9672308900b8b32ecd376381db5a1bf5519df7e
SpeeritX <Jedrek.185@gmail.com>
2021-08-24 22:11:31 +0200

    Fix bad merge

--- app/src/main/java/com/nqproject/MoneyApp/ui/screens/expense_list/ExpenseListScreen.kt
+++ app/src/main/java/com/nqproject/MoneyApp/ui/screens/expense_list/ExpenseListScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
